2203f07c3de32ce3d427925064498b882b861441
gean_ <gean.souza017a@gmail.com>
2024-08-02 05:41:45 -0300

    Continuing building header design - Search Bar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,25 +1,32 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { NavigationMenuComponent } from "./navigation-menu/navigation-menu.component";
 import { UserMenuComponent } from "./user-menu/user-menu.component";
 import Icon from '../../domain/entities/icon.entity';
 import Image from '../../domain/entities/image.entity';
 import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavigationMenuComponent, UserMenuComponent,CommonModule],
+  imports: [NavigationMenuComponent, UserMenuComponent,CommonModule,FormsModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css', '../global.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
   searchIcon = new Icon('Search_Icon',new Image('search-icon.svg','Search Icon'))
 
-  @Input() enableSearchBar = false;
-
+  enableSearchBar = false;
+  searchInput = ''
   
-  showSearchBar(){
-    this.enableSearchBar = !this.enableSearchBar;
+  showSearchBar(){console.log(this.searchInput)
+    if(this.searchInput == '') this.enableSearchBar = !this.enableSearchBar;
   }
-
+  ngOnInit(): void{};
 }
