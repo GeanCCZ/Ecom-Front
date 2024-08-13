41261634b0026f90810412ec367fe219d8dc2296
gean_ <gean.souza017a@gmail.com>
2024-08-12 23:44:49 -0300

    Style: Initial navigation style

--- src/app/components/header/navigation-menu/navigation-menu.component.ts
+++ src/app/components/header/navigation-menu/navigation-menu.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import Icon from '../../../domain/entities/icon.entity';
 import Image from '../../../domain/entities/image.entity';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navigation-menu',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule,CommonModule],
   templateUrl: './navigation-menu.component.html',
   styleUrls: ['./navigation-menu.component.css','../../global.component.css']
 })
@@ -15,4 +16,11 @@ export class NavigationMenuComponent {
 
   navigationMenuIcon = new Icon('Navigation-Menu_Icon',new Image('menu-icon.svg','Navigation menu icon'))
 
+  @Output() enableSideBar = false;
+
+  showSideBar(){
+    console.log(``)
+    this.enableSideBar = !this.enableSideBar
+    console.log(this.enableSideBar)
+  }
 }
